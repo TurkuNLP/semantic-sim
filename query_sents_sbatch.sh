1c164f8975bf0654450af68c90b264395502d12e
Emil Nuutinen <emil@emilnuutinen.com>
2022-11-22 16:40:50 +0200

    Formatting

--- query_sents_sbatch.sh
+++ query_sents_sbatch.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --account=Project_2002820
-#SBATCH --time=6:15:00
-##SBATCH --time=00:15:00
-#SBATCH --partition=gpu
+#SBATCH --account=Project_2005072
+#SBATCH --time=00:15:00
+#SBATCH --partition=gputest
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=6G
-#SBATCH --gres=gpu:v100:1,nvme:60
+#SBATCH --gres=gpu:v100:1,nvme:5
 #SBATCH --cpus-per-task=4
 
 
@@ -13,9 +12,7 @@ module load pytorch/1.11
 
 whichmodel=sbert
 
-DATADIR=/scratch/project_2000539/pb_faiss
+DATADIR=/scratch/project_2005072/emil/faiss_distractors
 
-zcat /scratch/project_2000539/pb_faiss/datain/para_sents.txt.gz | python3 print_nearest_from_faiss_index.py --bert-model TurkuNLP/bert-base-finnish-cased-v1 --fais ~/proj_finnlp_scratch/pb_faiss/faiss_index_filled_bert.faiss --out ~/proj_finnlp_scratch/pb_faiss/nearest_bert.ipkl
-
-zcat /scratch/project_2000539/pb_faiss/datain/para_sents.txt.gz | python3 print_nearest_from_faiss_index.py --sbert-model /scratch/project_2000539/pb_faiss/sbert-cased-finnish-paraphrase --sbert-tokenizer TurkuNLP/bert-base-finnish-cased-v1 --fais ~/proj_finnlp_scratch/pb_faiss/faiss_index_filled_sbert.faiss --out ~/proj_finnlp_scratch/pb_faiss/nearest_sbert.ipkl
+zcat /scratch/project_2005072/emil/faiss_distractors/datain/para_sents.txt.gz | python3 print_nearest_from_faiss_index.py --sbert-model sentence-transformers/paraphrase-multilingual-mpnet-base-v2 --sbert-tokenizer sentence-transformers/paraphrase-multilingual-mpnet-base-v2 --fais /scratch/project_2005072/emil/faiss_distractors/faiss_index_filled_sbert.faiss --out /scratch/project_2005072/emil/faiss_distractors/nearest_sbert.ipkl
 
